3a3bf87ea2371b9c11105cf7c5400d61f10fbdd8
Serafeim Papastefanos <spapas@gmail.com>
2018-02-22 18:17:01 +0200

    Start adding functionality

--- core/urls.py
+++ core/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 urlpatterns = [
     path('', core.views.HomeTemplateView.as_view(), name='home', ),
     path('person_list', core.views.PersonListView.as_view(), name='person_list', ),
+    path('modern_person_list', core.views.PersonModernListView.as_view(), name='modern_person_list', ),
     path('person_create', core.views.PersonCreateView.as_view(), name='person_create', ),
 ]
--- core/views.py
+++ core/views.py
@@ -10,8 +10,27 @@ class HomeTemplateView(TemplateView):
 
 class PersonListView(ListView):
     model = core.models.Person
+    paginate_by = 9
+
+
+class PersonModernListView(ListView):
+    model = core.models.Person
+    paginate_by = 9
+    template_name="core/modern_person_list.html"
+
+    def get_template_names(self):
+        if self.request.is_ajax():
+            return 'core/partial/person_page.html'
+
+        return super().get_template_names()
 
 
 class PersonCreateView(CreateView):
     model = core.models.Person
     fields = ('first_name', 'last_name', 'sex', )
+
+    def get_template_names(self):
+        if self.request.is_ajax():
+            return 'core/partial/person_form.html'
+
+        return super().get_template_names()
